cce8c75770fda69dd302b3da49bf94547c8dc3f2
vlada <vladasabelnikova@gmail.com>
2022-03-17 02:18:50 +0300

    Убрала рога
    Всё.

--- main.py
+++ main.py
@@ -143,5 +143,5 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT'), 5000)
+    port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
